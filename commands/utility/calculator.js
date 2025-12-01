8c89c21eaf8b7a9efdf49b0fdb4dfec85669f5d5
Sadeq86 <azizijami2@gmail.com>
2025-12-01 22:55:03 +0330

    Update calculator.js

--- commands/utility/calculator.js
+++ commands/utility/calculator.js
@@ -1,33 +1,32 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, ChannelType } = require('discord.js');
 const { joinVoiceChannel, getVoiceConnection } = require('@discordjs/voice');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('join')
-    .setDescription('Nasl-1 Will join a fixed voice channel.'),
+    .setDescription('Nasl-1 will join the fixed 24/7 voice channel'),
 
   async execute(interaction) {
     await interaction.deferReply();
 
-    
-    const FIXED_CHANNEL_ID = '1441136897836453963'; 
+    // آیدی چنل 24/7 خودت رو اینجا بذار
+    const FIXED_CHANNEL_ID = '1441136897836453963';
 
     const channel = interaction.guild.channels.cache.get(FIXED_CHANNEL_ID);
 
-    if (!channel || channel.type !== 2) {
-      return interaction.editReply({ content: 'Channel Not Found.' });
+    if (!channel || channel.type !== ChannelType.GuildVoice) {
+      return interaction.editReply({ content: 'Fixed voice channel not found!' });
     }
 
-    
     if (!channel.permissionsFor(interaction.guild.members.me).has(['Connect', 'Speak'])) {
-      return interaction.editReply({ content: 'I dont have access to join this channel.' });
+      return interaction.editReply({ content: 'I don\'t have permission to join this channel!' });
     }
 
-    
+    // اگه قبلاً تو چنل دیگه بود، خارج شو
     const oldConnection = getVoiceConnection(interaction.guild.id);
     if (oldConnection) oldConnection.destroy();
 
-    
+    // جوین به چنل
     joinVoiceChannel({
       channelId: channel.id,
       guildId: interaction.guild.id,
@@ -36,6 +35,6 @@ module.exports = {
       selfMute: false,
     });
 
-    await interaction.editReply({ content: ` ${channel} I joined.` });
+    await interaction.editReply({ content: `Successfully joined ${channel}! 24/7 mode active` });
   },
 };
