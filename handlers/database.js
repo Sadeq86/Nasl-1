04428ec80967a00e5f1c7f0e402314895b0d7362
Sadeq86 <azizijami2@gmail.com>
2025-12-12 19:47:27 +0330

    Update database.js

--- handlers/database.js
+++ handlers/database.js
@@ -1,10 +1,8 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
-module.exports = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log(global.styles.infoColor('✅ Connected to MongoDB'));
-  } catch (error) {
-    console.error('❌ Failed to connect to MongoDB:', error);
-  }
-};
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb+srv://Nasl-1:@Mobin1391@nasl-1.wat6o4b.mongodb.net/?appName=Nasl-1", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log("Mongo Connected"))
+.catch(err => console.log(err));
