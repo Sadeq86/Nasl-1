d4947cc4890ebde542270383ca14c3e106dd804e
Sadeq86 <azizijami2@gmail.com>
2026-01-04 11:51:18 +0330

    Delete handlers/database.js

--- handlers/database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect("mongodb+srv://Nasl-1:@Mobin1391@nasl-1.wat6o4b.mongodb.net/?appName=Nasl-1", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() => console.log("Mongo Connected"))
-.catch(err => console.log(err));
